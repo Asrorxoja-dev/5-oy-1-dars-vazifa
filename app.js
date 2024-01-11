e5ff38f66831b2887e94a846bced4e914c71fb66
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2024-01-11 18:31:34 +0500

    vasifa bajarildi

---
+++ app.js
@@ -0,0 +1,82 @@
+
+
+// 1-masala
+
+
+// const arr = [1, "salom", 2, "hello", 3,  4, "world", 5];
+
+// const numbers = arr.filter(element => typeof element === "number");
+
+// const sum = numbers.reduce( (total, value) => total + value, 0);
+
+// console.log(sum); // 15
+
+
+
+
+// 2-masala
+
+// let sozlar = ("lorem ipsum" );
+// let result = sozlar[0, 6].toUpperCase();
+// console.log(result);
+
+
+// function capitalizeWords(str) {
+//     let words = str.split(" ");
+//     let capitalizedWords = words.map(word => {
+//       let firstLetter = word[0].toUpperCase();
+//       let restOfWord = word.slice(1).toLowerCase();
+//       return firstLetter + restOfWord;
+//     });
+//     return capitalizedWords.join(" ");
+//   }
+  
+//   console.log(capitalizeWords("salom dunyo")); 
+
+
+// 3-masala
+
+
+// function checkPalindrome(string) {
+//     let reversedString = string.split("").reverse().join("");
+//     if (string == reversedString) {
+//       return "Palindrom";
+//     } else {
+//       return "Palindrom emas";
+//     }
+//   }
+
+//   let string = "radar";
+//   let result = checkPalindrome(string);
+//   console.log(result); 
+
+
+// 4-masala
+
+// const products = [
+//      { id: 1,
+//         name: "John",
+//         age:40
+//       },
+
+//       { id: 2,
+//         name: "Bill",
+//         age:45
+       
+//         },
+        
+      
+
+//       { id: 3,
+//         name: "Adam",
+//         age:30
+//       },
+     
+//     ];
+    
+    
+//     console.log(products[1].name);
+    
+
+
+ 
